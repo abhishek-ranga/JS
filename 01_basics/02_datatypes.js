9d5fde16e5a9473d6ff56ee91f81bfbc5d28e05b
Abhishek Ranga <abhishek07122004@gmail.com>
2025-07-17 04:52:12 +0000

    studied datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") // we are using nodejs not browser
+
+let name = "Abhishek" // string
+let age = 20 // number
+let isLoggedIn = true // boolean
+
+// number
+// bigint
+// string - ""
+// boolean - 1/0
+// null - datatype as well as standalone value - emptiness
+// undefined - declared but not initialised/ assigned
+// symbol - unique stuff
+
+// object
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
